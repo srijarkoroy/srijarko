a41b4c52adf69c636d4769e76388de1c40808f59
srijarkoroy <srijarko@gmail.com>
2025-01-19 19:31:46 +0530

    style: light theme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import streamlit.components.v1 as html
+from streamlit_theme import st_theme
 
 from utils.home import *
 from utils.skills import *
@@ -21,17 +22,31 @@ with st.sidebar:
     """
 
     st.markdown(html_temp, unsafe_allow_html=True)
+
     
-    opt = option_menu("Srijarko Roy", ["Home", "Experience", "Skills", "Education", "Certifications", "Research and Projects", "Publications"],
+    theme = st_theme()
+    if theme["base"] == "light":
+        opt = option_menu("Srijarko Roy", ["Home", "Experience", "Skills", "Education", "Certifications", "Research and Projects", "Publications"],
                          icons=['house', 'bookmarks', 'code-slash', 'book', 'award','kanban','journal-bookmark'],
                          menu_icon="app-indicator", default_index=0,
                          styles={
         "container": {"padding": "5!important", "background-color": "#0e117"},
         "icon": {"color": "#FF4B4B", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#262730"},
-        "nav-link-selected": {"background-color": "#262730"},
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#d3d3d3"},
+        "nav-link-selected": {"background-color": "#898c8a"},
     }
     )
+    else:
+        opt = option_menu("Srijarko Roy", ["Home", "Experience", "Skills", "Education", "Certifications", "Research and Projects", "Publications"],
+                            icons=['house', 'bookmarks', 'code-slash', 'book', 'award','kanban','journal-bookmark'],
+                            menu_icon="app-indicator", default_index=0,
+                            styles={
+            "container": {"padding": "5!important", "background-color": "#0e117"},
+            "icon": {"color": "#FF4B4B", "font-size": "25px"}, 
+            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#262730"},
+            "nav-link-selected": {"background-color": "#262730"},
+        }
+        )
 
     html_temp = """
         <div>
@@ -43,14 +58,25 @@ with st.sidebar:
     st.header("")
 
     left_co, cent1_co, cent2_co, cent3_co, cent4_co, last_co = st.columns(6)
-    with cent1_co:
-        st.markdown("[![Git](https://img.icons8.com/?size=35&id=106562&format=png&color=FFFFFF)](https://github.com/srijarkoroy)")
-    with cent2_co:
-        st.markdown("[![Link](https://img.icons8.com/?size=35&id=98960&format=png&color=FFFFFF)](https://www.linkedin.com/in/srijarko-roy-9193751b0/)")
-    with cent3_co:
-        st.markdown("[![Insta](https://img.icons8.com/?size=35&id=32320&format=png&color=FFFFFF)](https://www.instagram.com/srijarko/)")
-    with cent4_co:
-        st.markdown("[![Med](https://img.icons8.com/?size=35&id=bocK2vOACVtF&format=png&color=FFFFFF)](https://medium.com/@srijarkoroy4u)")
+
+    if theme["base"] == "light":
+        with cent1_co:
+            st.markdown("[![Git](https://img.icons8.com/?size=35&id=16318&format=png&color=898c8a)](https://github.com/srijarkoroy)")
+        with cent2_co:
+            st.markdown("[![Link](https://img.icons8.com/?size=35&id=98960&format=png&color=898c8a)](https://www.linkedin.com/in/srijarko-roy-9193751b0/)")
+        with cent3_co:
+            st.markdown("[![Insta](https://img.icons8.com/?size=35&id=32320&format=png&color=898c8a)](https://www.instagram.com/srijarko/)")
+        with cent4_co:
+            st.markdown("[![Med](https://img.icons8.com/?size=35&id=bocK2vOACVtF&format=png&color=898c8a)](https://medium.com/@srijarkoroy4u)")
+    else:
+        with cent1_co:
+            st.markdown("[![Git](https://img.icons8.com/?size=35&id=16318&format=png&color=d3d3d3)](https://github.com/srijarkoroy)")
+        with cent2_co:
+            st.markdown("[![Link](https://img.icons8.com/?size=35&id=98960&format=png&color=d3d3d3)](https://www.linkedin.com/in/srijarko-roy-9193751b0/)")
+        with cent3_co:
+            st.markdown("[![Insta](https://img.icons8.com/?size=35&id=32320&format=png&color=d3d3d3)](https://www.instagram.com/srijarko/)")
+        with cent4_co:
+            st.markdown("[![Med](https://img.icons8.com/?size=35&id=bocK2vOACVtF&format=png&color=d3d3d3)](https://medium.com/@srijarkoroy4u)")
 
 if opt == "Home":
     home()
